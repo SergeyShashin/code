410020f743738b66d265ae66f23232bcd12cc2e4
SergeyShashin <s9997910@yandex.ru>
2024-11-28 16:22:33 +0300

    Code. 4_jsPrStGGC done.

--- js/practice/4_jsPrStGGC/4_jsPrStGGC.js
+++ js/practice/4_jsPrStGGC/4_jsPrStGGC.js
@@ -4,16 +4,64 @@
  * Давайте теперь реализуем игру угадай ячейку. В этой игре будет дана таблица 10 на 10.
  * Компьютер случайным образом запоминает 10 ячеек из этой таблицы.
  * Игроку нужно кликать на клетки пока он не найдет все загаданные компьютером клетки.
+ * Добавить таймер обратного отсчета. Игроку нужно успеть угадать за отведенное время.
  */
 
 const settings = {
   rowsCount: 10,
   colsCount: 10,
-  quantityRandomNumbers: 10
+  quantityRandomNumbers: 10,
+  limitTime: 88,
 };
+
+const gameTimer = {
+  timerEl: null,
+  timer: null,
+  numberInterval: null,
+
+  init(el, number) {
+    this.timerEl = el;
+    this.set(number);
+  },
+
+  set(number) {
+    this.timer = number;
+    this.render();
+  },
+
+  render() {
+    this.timerEl.textContent = this.timer;
+  },
+
+  start(number) {
+    this.timer = number;
+    this.numberInterval = setInterval(() => {
+      if (this.timer === 0) {
+        this.stop();
+        return
+      }
+      this.timer--;
+      this.render()
+    }, 1000)
+  },
+
+  stop() {
+    clearInterval(this.numberInterval);
+    this.render();
+  },
+
+  getNumberInterval() {
+    return this.numberInterval;
+  },
+
+  getNumberTimer() {
+    return this.timer
+  }
+}
 
 const guesCell = {
   settings,
+  gameTimer,
   guessCellEl: null,
   playingFieldEl: null,
   newGame: null,
@@ -27,6 +75,7 @@ const guesCell = {
     this.guessCellEl = document.getElementById('guessCell');
     this.playingFieldEl = document.getElementById('playingField');
     this.newGameEl = document.getElementById('newGame');
+    this.gameTimer.init(document.getElementById('timer'), this.settings.limitTime);
     this.setEventHandlers();
     this.run();
   },
@@ -43,6 +92,8 @@ const guesCell = {
     this.randomNumbers = this.createRandomNumbers();
     this.createPlayingField();
     this.statusPlaying = 'play';
+    clearInterval(this.gameTimer.getNumberInterval());
+    this.gameTimer.start(this.settings.limitTime);
   },
 
   createRandomNumbers() {
@@ -77,7 +128,7 @@ const guesCell = {
   },
 
   guesCellHandler(e) {
-    if (e.target.tagName !== 'TD' || this.statusPlaying === 'finish') {
+    if (e.target.tagName !== 'TD' || this.statusPlaying === 'finish' || this.statusPlaying === 'timeEnd') {
       return
     }
 
@@ -89,10 +140,22 @@ const guesCell = {
       tdEl.classList.add('guessed');
       this.quantityGuessed++;
       this.quantityGuessed === this.settings.quantityRandomNumbers ? this.statusPlaying = 'finish' : '';
+    } else {
+      tdEl.classList.add('clicked');
+    }
+
+    if (this.gameTimer.getNumberTimer() === 0) {
+      this.statusPlaying = 'timeEnd';
+    }
+
+    if (this.statusPlaying === 'timeEnd') {
+      alert(`Время угадывания закончилось.`);
+      return
     }
 
     if (this.statusPlaying === 'finish') {
-      alert(`Все загаданные ячейки угаданы за ${this.attempts} попыток)`);
+      this.gameTimer.stop();
+      alert(`Все загаданные ячейки угаданы c ${this.attempts} попытки.)`);
     }
 
   }
