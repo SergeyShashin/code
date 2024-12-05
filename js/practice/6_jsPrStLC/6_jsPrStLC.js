71317f52479674a8e584662d533f50c14c1b631d
SergeyShashin <s9997910@yandex.ru>
2024-12-05 12:07:17 +0300

    Code. 6_jsPrStLC update.

--- js/practice/6_jsPrStLC/6_jsPrStLC.js
+++ js/practice/6_jsPrStLC/6_jsPrStLC.js
@@ -10,12 +10,16 @@
 
 const lineCalendar = {
   lineCalendarEl: null,
+  yearEl: null,
+  monthEl: null,
+  dayWeekEl: null,
   date: null,
   currentYear: null,
   nameCurrentMonth: null,
   currentNumberMonth: null,
   currentMonth: null,
-  nameCurrentDay: null,
+  numberCurrentDayMonth: null,
+  nameCurrentDayWeek: null,
   namesMonths: null,
   namesDays: null,
   leapYear: null,
@@ -23,23 +27,31 @@ const lineCalendar = {
 
   init() {
     this.lineCalendarEl = document.getElementById('lineCalendar');
+    this.yearEl = document.getElementById('year');
+    this.monthEl = document.getElementById('month');
+    this.dayWeekEl = document.getElementById('dayWeek');
     this.namesMonths = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
     this.namesDays = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
     this.quantityDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     this.quantityDaysInFebruaryOfLeapYear = 29;
     this.date = new Date();
     this.currentYear = this.date.getFullYear();
-    this.currentNumberMonth = this.date.getDay();
+    this.currentNumberMonth = this.date.getMonth();
     this.nameCurrentMonth = this.namesMonths[this.currentNumberMonth];
-    this.nameCurrentDay = this.namesDays[this.date.getDay()];
+    this.nameCurrentDayWeek = this.namesDays[this.date.getDay()];
     this.leapYear = this.defineLeapYear();
     this.currentMonth = this.getCurrentMonth();
+    this.numberCurrentDayMonth = this.date.getDate();
 
     console.log(this.currentYear);
     console.log(this.nameCurrentMonth);
-    console.log(this.nameCurrentDay);
+    console.log(this.nameCurrentDayWeek);
     console.log(this.leapYear);
     console.log(this.currentMonth);
+    console.log('текущий день месяца ' + this.numberCurrentDayMonth);
+
+    this.setYearAndMonthTohtml();
+    this.addCurrentMonthToHTML();
   },
 
   defineLeapYear() {
@@ -53,9 +65,38 @@ const lineCalendar = {
       ? this.quantityDaysInFebruaryOfLeapYear
       : this.quantityDaysInMonth[this.currentNumberMonth];
 
-    console.log('дней в этом месяце' + quantityDaysInMonth);
+    for (let day = 1; day <= quantityDaysInMonth; day++) {
+      currentMonth.push(day);
+    }
 
-    // for (let day = 1; day < this) { }
+    return currentMonth;
+  },
+
+  addCurrentMonthToHTML() {
+    for (let day of this.currentMonth) {
+      let liEl = document.createElement('li');
+      liEl.textContent = day;
+      liEl.dataset.currentYear = this.currentYear;
+      liEl.dataset.currentMonth = this.currentMonth;
+      liEl.dataset.currentMonth = this.num;
+      liEl.dataset.currentNumberMonth = this.currentNumberMonth;
+
+      if (this.numberCurrentDayMonth === day) {
+        liEl.classList.add('currentDayMonth');
+        liEl.dataset.currentDayMonth = this.numberCurrentDayMonth;
+      } else if (this.numberCurrentDayMonth < day) {
+        liEl.classList.add('pastDaysCurrentMonth');
+      } else if (this.numberCurrentDayMonth > day) {
+        liEl.classList.add('futureDaysCurrentMonth');
+      }
+      this.lineCalendarEl.appendChild(liEl);
+    }
+  },
+
+  setYearAndMonthTohtml() {
+    this.yearEl.textContent = this.currentYear;
+    this.monthEl.textContent = this.nameCurrentMonth;
+    this.dayWeekEl.textContent = this.nameCurrentDayWeek;
   }
 
 };
