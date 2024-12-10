2ba2c7d5475d5bf2ee0913c7d7ae5644e9a34629
SergeyShashin <s9997910@yandex.ru>
2024-12-10 09:44:43 +0300

    Code. 8_jsPrStHS done.

--- js/practice/8_jsPrStHS/8_jsPrStHS.js
+++ js/practice/8_jsPrStHS/8_jsPrStHS.js
@@ -12,6 +12,8 @@ const searchHistoricalEventsByYear = {
   monitorEl: null,
   historicalEventsEl: null,
   historicalEvents: null,
+  messages: null,
+
   init() {
     this.searchHistoricalEventsByYear = document.getElementById('searchHistoricalEventsByYear');
     this.inputEl = document.getElementById('input');
@@ -23,9 +25,73 @@ const searchHistoricalEventsByYear = {
         date: '01.01.24',
         event: 'название события 01.01.24',
         description: 'описание события 01.01.24'
-      }
+      },
+      2023: {
+        date: '01.01.23',
+        event: 'название события 01.01.23',
+        description: 'описание события 01.01.23'
+      },
+      2022: {
+        date: '01.01.22',
+        event: 'название события 01.01.22',
+        description: 'описание события 01.01.22'
+      },
     };
+    this.messages = {
+      notInformationTag: 'Информации об этом событии на данный момент нет.'
+    }
+    this.appendOptionInDatalist();
+    this.setEventHandlers();
+  },
+
+  appendOptionInDatalist() {
+    let years = Object.keys(this.historicalEvents);
+
+    for (let year of years) {
+      let optionEl = document.createElement('option');
+      optionEl.value = year;
+      historicalEvents.appendChild(optionEl);
+    }
+  },
+
+  setEventHandlers() {
+    this.btnFindEl.addEventListener('click', e => this.handlerClickBtnFindEl(e));
+    document.addEventListener('keydown', e => this.handlerKeydown(e));
+  },
+
+  handlerClickBtnFindEl(e) {
+    let targetClick = e.target;
+
+    if (targetClick.tagName === 'BUTTON') {
+      this.outputInfoEvent();
+    }
+  },
+
+  outputInfoEvent() {
+    let query = this.inputEl.value;
+    if (query && this.historicalEvents[query]) {
+      let trEl = document.createElement('tr');
+      let tdDateEventEl = document.createElement('td');
+      let tdNameEventEl = document.createElement('td');
+      let tdDescriptionEventEl = document.createElement('td');
+      tdDateEventEl.textContent = this.historicalEvents[query].date;
+      tdNameEventEl.textContent = this.historicalEvents[query].event;
+      tdDescriptionEventEl.textContent = this.historicalEvents[query].description;
+      trEl.appendChild(tdDateEventEl);
+      trEl.appendChild(tdNameEventEl);
+      trEl.appendChild(tdDescriptionEventEl);
+      this.monitorEl.appendChild(trEl);
+    } else {
+      this.monitorEl.textContent = this.messages['notInformationTag']
+    }
+  },
+
+  handlerKeydown(e) {
+    if (e.code === 'Enter') {
+      this.outputInfoTag();
+    }
   }
+
 };
 
 window.onload = searchHistoricalEventsByYear.init();
