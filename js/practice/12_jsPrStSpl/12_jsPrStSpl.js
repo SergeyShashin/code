26c8db7f3a4cd85927b39d97e735732b2d92d74b
SergeyShashin <s9997910@yandex.ru>
2024-12-13 18:08:26 +0300

    Code. 12_jsPrStSpl start.

---
+++ js/practice/12_jsPrStSpl/12_jsPrStSpl.js
@@ -0,0 +1,62 @@
+/**
+Сейчас мы научимся делать спойлеры. Под спойлерами я понимаю блоки, которые можно развернуть, либо свернуть с помощью специальной ссылки.
+
+В следующем примере кода абзац с классом spoiler является спойлером, а ссылка с классом toggle должна по клику показывать или скрывать этот спойлер:
+ <p>
+  абзац с текстом
+</p>
+<p>
+  абзац с текстом
+  <a href="" class="toggle">развернуть спойлер 1</a>
+</p>
+<p class="spoiler">
+  скрытый спойлер 1
+</p>
+<p>
+  абзац с текстом
+</p>
+<p>
+  абзац с текстом
+  <a href="" class="toggle">развернуть спойлер 2</a>
+</p>
+<p class="spoiler">
+  скрытый спойлер 2
+</p>
+<p>
+  абзац с текстом
+</p>
+Очевидно, что ссылку для разворачивания и сам спойлер надо как-то связать. Можно придумать различные варианты, но в данном случае я предлагаю сделать по местоположению: скажем, что ссылка с классом toggle открывает или закрывает элемент с классом spoiler, расположенный сразу под родителем этой ссылки.
+
+Пусть спойлер по умолчанию будет скрыт, а если мы хотим его показать - будем давать ему класс active. Напишем соответствующий CSS код:
+.spoiler:not(.active) {
+  display: none;
+}
+ 
+ */
+
+let spoiler = {
+  spoilerEl: null,
+  spoilersEl: null,
+
+  init() {
+    this.spoilerEl = document.getElementById('spoiler');
+    this.spoilersEl = document.querySelectorAll('.spoiler');
+    this.setEventHandlers();
+  },
+
+  setEventHandlers() {
+    this.spoilerEl.addEventListener('click', (e) => this.handlerClickSploilerEl(e));
+  },
+
+  handlerClickSploilerEl(e) {
+    if (e.target.classList.contains('toggle')) {
+      let text = e.target.textContent.split(' ');
+      console.log(text);
+
+      console.log(this.spoilersEl);
+    }
+  }
+};
+
+window.onload = spoiler.init();
+
