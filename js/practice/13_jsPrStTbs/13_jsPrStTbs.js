e693f8be15b9ada85b0b13f6843c385a5efa50e1
SergeyShashin <s9997910@yandex.ru>
2024-12-16 18:00:26 +0300

    Code. 13_jsPrStTbs updated.

--- js/practice/13_jsPrStTbs/13_jsPrStTbs.js
+++ js/practice/13_jsPrStTbs/13_jsPrStTbs.js
@@ -2,11 +2,35 @@
 
 let tabs = {
   tabsEl: null,
-  tabsMenu: null,
+  tabsMenuEl: null,
+  textEl: null,
   tabsArr: null,
   textArr: null,
+  numTabclasActive: null,
 
   init() {
+    this.tabsEl = document.getElementById('tabs');
+    this.tabsMenuEl = document.getElementById('tabsMenu');
+    this.textEl = document.getElementById('text');
+    this.tabsArr = this.tabsMenuEl.querySelectorAll('a');
+    this.textArr = this.textEl.querySelectorAll('p');
 
+    this.setEventHandlers();
+  },
+
+  setEventHandlers() {
+    this.tabsMenuEl.addEventListener('click', e => this.handlerClickTab(e));
+  },
+
+  handlerClickTab(e) {
+    let target = e.target;
+
+    if (target.tagName !== 'A') {
+      return
+    }
+
+    console.log(target);
   }
-}
+};
+
+window.onload = tabs.init();
