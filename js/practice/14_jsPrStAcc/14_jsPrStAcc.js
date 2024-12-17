01002ffdac4ea963dd461dd36e83209432437b3a
SergeyShashin <s9997910@yandex.ru>
2024-12-17 11:51:56 +0300

    Code. 14_jsPrStAcc updated.

--- js/practice/14_jsPrStAcc/14_jsPrStAcc.js
+++ js/practice/14_jsPrStAcc/14_jsPrStAcc.js
@@ -3,12 +3,33 @@
 let accordion = {
   accordionEl: null,
   tabsEls: null,
-  numberActivelink: null,
+  numberActiveTab: null,
   init() {
     this.accordionEl = document.getElementById('accordion');
-    this.linksEls = document.querySelectorAll('tab');
-
+    this.tabsEls = document.querySelectorAll('.tab');
+    this.setEventHandlers();
   },
+
+  setEventHandlers() {
+    this.accordionEl.addEventListener('click', e => this.handlerClickTab(e));
+  },
+
+  handlerClickTab(e) {
+    let target = e.target;
+
+    if (target.className !== 'link') {
+      return
+    }
+
+    let numberClickTab = Number(target.parentElement.dataset.number);
+
+    if (this.numberActiveTab === numberClickTab) {
+
+    }
+
+    this.tabsEls[this.numberActiveTab].querySelector('.text').classList.add('activeText');
+  }
+
 };
 
 window.onload = accordion.init();
