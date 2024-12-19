7f388b4b361b372dddb012b1067a428416bf3f6d
SergeyShashin <s9997910@yandex.ru>
2024-12-19 16:20:44 +0300

    Code. 30_jsPrTst updated.

--- js/practice/30_jsPrTst/30_jsPrTst.js
+++ js/practice/30_jsPrTst/30_jsPrTst.js
@@ -4,13 +4,14 @@ let test = {
   testEl: null,
   questions: null,
   quantityCorrectAnswerUser: null,
+  msgs: null,
 
   init() {
     this.testEl = document.getElementById('test');
     this.questions = {
       0: {
-        numberQuestion: 0,
-        question: 'Какой номер вопроса?',
+        questionNumber: 0,
+        questionText: 'Какой номер вопроса?',
         answerUser: null,
         correctAnswer: 0,
         resultAnswer: null
@@ -30,8 +31,46 @@ let test = {
         resultAnswer: null
       },
     };
+    this.msgs = {
+      notAnswer: 'Можно ответить.',
+      correctAnswer: 'Ответ совпадает с верным:)',
+      answerUser: 'Дан индивидуальный ответ:)'
+    };
     this.quantityCorrectAnswerUser = 0;
+    this.addQuestionsHTML();
+  },
 
+  addQuestionsHTML() {
+    
+    for (let key in this.questions) {
+      let questionEl = document.createElement('article');
+      let headerEl = document.createElement('header');
+      let h4El = document.createElement('h4');
+      let pEl = document.createElement('p');
+      let inputEl = document.createElement('input');
+      let preEl = document.createElement('pre');
+
+      questionEl.id = key;
+
+      h4El.textContent = this.questions[key].questionNumber;
+      pEl.textContent = this.questions[key].questionText;
+      preEl.textContent = this.msgs.notAnswer;
+
+      questionEl.classList.add('question');
+      pEl.classList.add('questionText');
+      inputEl.classList.add('answerUser');
+      preEl.classList.add('resultAnswer');
+
+      this.addElHTML(this.testEl, headerEl);
+      this.addElHTML(headerEl, h4El);
+      this.addElHTML(this.testEl, pEl);
+      this.addElHTML(this.testEl, inputEl);
+      this.addElHTML(this.testEl, preEl);
+
+    }
+  },
+  addElHTML(parent, el) {
+    parent.appendChild(el);
   }
 
 };
