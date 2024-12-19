73cbebfafcadcf62b5d8f6a430b9877e537f6565
SergeyShashin <s9997910@yandex.ru>
2024-12-19 17:42:39 +0300

    Code. 30_jsPrTst updated.

--- js/practice/30_jsPrTst/30_jsPrTst.js
+++ js/practice/30_jsPrTst/30_jsPrTst.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * Источник https://code.mu/ru/javascript/book/practice/test/html-questions-answers/
+ * В этом и в нескольких следующих уроках мы с вами займемся созданием тестов с вопросами и ответами.
+ * Начнем с самых простых вариантов и будем постепенно усложнять.
+ * Пусть для начала мы хотим сделать набор вопросов, на каждый из которых возможен только один ответ.
+ * Пусть ответы вбиваются пользователем в инпуты под самими вопросами.
+ */
+
 let test = {
   testEl: null,
   questions: null,
@@ -13,21 +21,21 @@ let test = {
         questionNumber: 0,
         questionText: 'Какой номер вопроса?',
         answerUser: null,
-        correctAnswer: 0,
+        correctAnswer: '0',
         resultAnswer: null
       },
       1: {
         questionNumber: 1,
         question: 'Какой номер вопроса?',
         answerUser: null,
-        correctAnswer: 1,
+        correctAnswer: '1',
         resultAnswer: null
       },
       2: {
         questionNumber: 2,
         question: 'Какой номер вопроса?',
         answerUser: null,
-        correctAnswer: 2,
+        correctAnswer: '2',
         resultAnswer: null
       },
     };
@@ -38,6 +46,7 @@ let test = {
     };
     this.quantityCorrectAnswerUser = 0;
     this.addQuestionsHTML();
+    this.setEventHandlers();
   },
 
   addQuestionsHTML() {
@@ -60,6 +69,7 @@ let test = {
       pEl.classList.add('questionText');
       inputEl.classList.add('answerUser');
       preEl.classList.add('resultAnswer');
+      preEl.id = `${key}_resultAnswer`;
 
       this.addElHTML(this.testEl, questionEl);
       this.addElHTML(questionEl, headerEl);
@@ -70,9 +80,33 @@ let test = {
 
     }
   },
-  
+
   addElHTML(parent, el) {
     parent.appendChild(el);
+  },
+
+  setEventHandlers() {
+    this.testEl.addEventListener('change', e => this.handleChangeTestEl(e));
+  },
+
+  handleChangeTestEl(e) {
+    let target = e.target;
+    let numberQuestion = target.parentElement.id;
+    let answerUser = target.value;
+    let resultAnswer = this.checkAnswerUser(numberQuestion, answerUser);
+    console.log(resultAnswer);
+    this.setResultHtml(numberQuestion, resultAnswer);
+  },
+
+  checkAnswerUser(numberQuestion, answerUser) {
+    return this.questions[numberQuestion].correctAnswer === answerUser;
+  },
+
+  setResultHtml(numberQuestion, resultAnswer) {
+    let resultEl = document.getElementById(`${numberQuestion}_resultAnswer`);
+    console.log(resultEl);
+
+    resultEl.textContent = resultAnswer ? this.msgs.correctAnswer : this.msgs.answerUser;
   }
 
 };
