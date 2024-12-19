23d3e818ddfaf0fa6c9d7d4428c33f921d1b0c48
SergeyShashin <s9997910@yandex.ru>
2024-12-19 12:37:23 +0300

    Code. 24_jsPrPC updated.

--- js/practice/24_jsPrPC/24_jsPrPC.js
+++ js/practice/24_jsPrPC/24_jsPrPC.js
@@ -4,12 +4,12 @@ let productCalculator = {
   productCalculatorEl: null,
   enterEl: null,
   inputsElsInEnterEl: null,
-  // nameInputEl: null,
-  // priceInputEl: null,
-  // quantityInputEl: null,
+  trEls: null,
+  tdEls: null,
   addBtnEl: null,
   monitorEl: null,
   resultEl: null,
+  counterForTrElId: null,
 
   init() {
 
@@ -18,8 +18,12 @@ let productCalculator = {
     this.addBtnEl = document.getElementById('add');
     this.monitorEl = document.getElementById('monitor');
     this.resultEl = document.getElementById('result');
+    this.trEls = {};
+    this.tdEls = {};
+    this.counterForTrElId = 0;
 
     this.setEventHandlers();
+
   },
 
   setEventHandlers() {
@@ -53,17 +57,47 @@ let productCalculator = {
 
     trEl.appendChild(this.createTdEl('sum', sum));
     trEl.appendChild(this.createTdEl('remove', 'удалить'));
-    
+
+    trEl.dataset.code = this.counterForTrElId;
+    this.trEls[this.counterForTrElId] = trEl;
     this.monitorEl.appendChild(trEl);
+
+    this.renderResult(this.getResultSum());
+    this.counterForTrElId++;
   },
 
   createTdEl(idInput, contentInput) {
     let tdEl = document.createElement('td');
-    tdEl.textContent = contentInput;
+
+    if (idInput !== 'sum' && idInput !== 'remove') {
+      let inputEl = document.createElement('input');
+      inputEl.value = contentInput;
+      tdEl.appendChild(inputEl);
+    } else {
+      tdEl.textContent = contentInput;
+    }
+
+    tdEl.dataset.code = `${this.counterForTrElId}_${idInput}`;
+
+    this.tdEls[`${this.counterForTrElId}_${idInput}`] = tdEl;
     tdEl.classList.add(idInput);
     return tdEl
-  }
+  },
 
+  getResultSum() {
+    let keysTdEls = Object.keys(this.tdEls);
+    let sum = 0;
+
+    for (let keyTdEls of keysTdEls) {
+      /(sum)/.test(keyTdEls) ? sum += Number(this.tdEls[keyTdEls].textContent) : '';
+    }
+
+    return sum;
+  },
+
+  renderResult(sum) {
+    this.resultEl.textContent = `Итого: ${sum}`;
+  }
 };
 
 window.onload = productCalculator.init();
