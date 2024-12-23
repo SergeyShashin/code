8461ffb82b657f33ee2bb2c9c64ed825ad9259e1
SergeyShashin <s9997910@yandex.ru>
2024-12-23 13:00:25 +0300

    Code. 47_jsPrGChC start.

---
+++ js/practice/47_jsPrGChC/47_jsPrGChC.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const settings = {
+  rowsQuantity: 3,
+  colsQuantity: 3,
+  colors: ['red', 'green', 'blue'],
+};
+
+const config = {
+  settings,
+
+  init(userSettings) {
+    this.settings = Object.assign(this.settings, userSettings);
+  },
+
+  getRows() {
+    return this.settings.rowsQuantity
+  },
+
+  getCols() {
+    return this.settings.colsQuantity
+  },
+
+  getColors() {
+    return this.settings.colors
+  }
+};
+
+const map = {
+  gameChangeColorEl: null,
+  rowsQuantity: null,
+  colsQuantityQuantity: null,
+  colors: null,
+  gameEls: null,
+
+  init(rowsQuantity, colsQuantity, colors) {
+    this.rowsQuantity = rowsQuantity;
+    this.colsQuantityQuantity = colsQuantity;
+    this.colors = [...colors];
+    this.gameChangeColorEl = document.getElementById('gameChangeColor');
+    this.gameEls = {};
+
+    for (let row = 0; row < rowsQuantity; row++) {
+      let trEl = document.createElement('tr');
+      for (let col = 0; col < colsQuantity; col++) {
+        let tdEl = document.createElement('td');
+        trEl.appendChild(tdEl);
+        this.gameEls[`${row}_${col}`] = tdEl;
+      }
+      this.gameChangeColorEl.appendChild(trEl);
+    }
+
+  },
+
+  getgameChangeColorEl() {
+    return this.gameChangeColorEl
+  },
+
+  getGameEls() {
+    return this.gameEls
+  },
+
+};
+
+const statusGame = {
+
+}
+
+const gameChangeColor = {
+  config,
+  map,
+  statusGame,
+
+  init(userSettings = {}) {
+    this.config.init(userSettings);
+    this.map.init(this.config.getRows(), this.config.getCols(), this.config.getColors());
+  }
+
+};
+
+window.onload = gameChangeColor.init({ rowsQuantity: 5, colsQuantity: 5 });
+
