77acfb565a1728c32a5c09f746f06690909475f1
SergeyShashin <s9997910@yandex.ru>
2025-01-21 14:56:12 +0300

    Code. 95_jsSpSgPrm notebook updated.

--- js/practice/95_jsSpSgPrm/notebook/notebook.js
+++ js/practice/95_jsSpSgPrm/notebook/notebook.js
@@ -7,7 +7,46 @@
  */
 
 const notebook = {
+  btnAddNoteEl: null,
+  notes: null,
+  noteNameEl: null,
+  noteTextEl: null,
+  noteLinksEl: null,
+
   run() {
-    console.log('старт.');
+    this.init();
+  },
+
+  init() {
+    this.btnAddNoteEl = document.getElementById('btnAddNote');
+    this.notes = {};
+    this.noteNameEl = document.getElementById('noteName');
+    this.noteTextEl = document.getElementById('noteText');
+    this.noteLinksEl = document.getElementById('noteLinks');
+
+    this.setEventHandlers();
+  },
+
+  setEventHandlers() {
+    this.btnAddNoteEl.addEventListener('click', () => this.handlerClickBtnAddNoteEl());
+  },
+
+  handlerClickBtnAddNoteEl() {
+    let noteLinkEl = this.createHTMLEl('li', 'a', this.noteNameEl.value);
+
+    this.notes[this.noteNameEl] = { noteName: this.noteNameEl.value, noteText: this.noteTextEl.value };
+
+    localStorage.setItem(this.noteNameEl.value, this.noteTextEl.value);
+
+    this.noteLinksEl.appendChild(noteLinkEl);
+  },
+
+  createHTMLEl(typeElement, typeSubElement, contentSubElement) {
+    let newEl = document.createElement(typeElement);
+    let newSubEl = document.createElement(typeSubElement);
+    newSubEl.textContent = contentSubElement;
+    typeSubElement === 'a' ? newSubEl.href = '#' : '';
+    newEl.appendChild(newSubEl);
+    return newEl;
   }
 }
