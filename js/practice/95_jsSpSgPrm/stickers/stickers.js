1c610e73db25624e26883ecb473c08c2085817d8
SergeyShashin <s9997910@yandex.ru>
2025-01-22 13:10:35 +0300

    Code. 95_jsSpSgPrm stickers updated.

--- js/practice/95_jsSpSgPrm/stickers/stickers.js
+++ js/practice/95_jsSpSgPrm/stickers/stickers.js
@@ -6,3 +6,45 @@
  * Каждый новый стикер должен добавляться в конец плиточки.
  */
 
+const stickers = {
+  stickersEl: null,
+  stickersDB: null,
+  counterStickers: null,
+
+  run() {
+    this.stickersEl = document.getElementById('stickers');
+    this.stickersDB = JSON.parse(localStorage.getItem('stickers'));
+    this.counterStickers = 0;
+    this.insertStickersInHTML();
+  },
+
+  insertStickersInHTML() {
+    for (let sticker of this.stickersDB) {
+      let stikerEl = new StickerEl(sticker.id, sticker.text);
+      this.stickersEl.appendChild(stikerEl);
+    }
+  }
+}
+
+function StickerEl(idStiker, textStiker) {
+  let article = document.createElement('article');
+  let headerEl = document.createElement('header');
+  let btnRemoveEl = document.createElement('button');
+  let textareaEl = document.createElement('textarea');
+
+  article.id = idStiker;
+  article.classList.add('sticker');
+
+  headerEl.appendChild(btnRemoveEl);
+
+  btnRemoveEl.textContent = 'X';
+  btnRemoveEl.classList.add('removeSticker');
+
+  textareaEl.value = textStiker;
+  textareaEl.classList.add('stickerText');
+
+  article.appendChild(headerEl);
+  article.appendChild(textareaEl);
+
+  return article
+}
