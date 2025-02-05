f3a14214255b5b716509246dc5408d8726054515
SergeyShashin <s9997910@yandex.ru>
2025-02-05 17:19:24 +0300

    Code. NodeJS16+ task 4 updated.

--- js/NodeJS/index.js
+++ js/NodeJS/index.js
@@ -19,3 +19,27 @@ let numbers = fs.readFileSync('numbers.txt', 'utf8');
 let numbers1 = fs.readFileSync('numbers1.txt', 'utf8');
 
 console.log(`Сумма numbers и numbers1 = ${[...numbers.split(' '), ...numbers1.split(' ')].reduce((acc, el) => acc + Number(el), 0)}`);
+
+// Дан объект:
+
+// let obj = {
+// 	'file1.txt': 'text1',
+// 	'file2.txt': 'text2',
+// 	'file3.txt': 'text3',
+// }
+// С помощью цикла для каждого элемента объекта создайте файл, именем которого будет свойство элемента,
+//  а текстом - значение свойства.
+
+let obj = {
+  'file1.txt': 'text1',
+  'file2.txt': 'text2',
+  'file3.txt': 'text3',
+}
+
+for (let key in obj) {
+  fs.writeFileSync(`${key}.txt`, `${obj[key]}`);
+}
+
+//3. Дан файл с текстом. Запустите таймер, который каждые 5 секунд в конец этого файла будет записывать восклицательный знак
+let sign = '!';
+// setInterval(() => { fs.writeFileSync('fileForSign.txt', sign); sign += '!' }, 5000);
