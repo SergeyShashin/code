9298c3415c2e2fd80b411f8616728a61cdf4d218
SergeyShashin <s9997910@yandex.ru>
2025-02-05 16:31:27 +0300

    Code. NodeJS16+ task 3 done.

--- js/NodeJS/index.js
+++ js/NodeJS/index.js
@@ -1,4 +1,11 @@
 import { square, cube } from './math.js';
+// import _ from 'underscore';
+import _ from 'lodash';
 
 let res = square(2) + cube(3);
 console.log(res);
+
+// console.log(_.first([1, 2, 3]));
+
+console.log(_.chunk([1, 2, 3], 2));
+console.log(_.compact([1, '', 2, '', 3]));
