81a2a3e71f74e52d7d1e41b8735f951b2176c122
SergeyShashin <s9997910@yandex.ru>
2025-02-12 18:03:38 +0300

    Code. NodeJS16+ 17_ndPmSvUn done.

--- js/NodeJS/17_ndPmSvUn/index.js
+++ js/NodeJS/17_ndPmSvUn/index.js
@@ -1,8 +1,14 @@
 import http, { request } from 'http';
 let i = 0;
 
+/**
+ * Пусть изначально наш счетчик имеет значение 100.
+ * Каждый запрос уменьшайте это значение на единицу и отдавайте новое значение в браузер.
+ * Как только счетчик дойдет до 100, выведите результатом запроса сообщение об этом.
+ */
+
 http.createServer((request, responce) => {
-  responce.writeHead(200, {'Content-Type': 'text/html'});
-  responce.write(`<h1>${i++}</h1>`);
+  responce.writeHead(200, {'Content-Type': 'text/html', 'Content-Language': 'ru'});
+  i===10 ?  responce.write(`<h1>Quantity ${i++}</h1>`):responce.write(`<h1>${i++}</h1>`);
   responce.end();
-});
+}).listen(3000);
