e32ae88fd8a741b1222d99baa8b59ee5f102ccf3
SergeyShashin <s9997910@yandex.ru>
2025-02-12 09:19:33 +0300

    Code. NodeJS16+ 12_ndPmFSECh done.

--- js/NodeJS/12_ndPmFSECh/index.js
+++ js/NodeJS/12_ndPmFSECh/index.js
@@ -1,4 +1,25 @@
 import fs from 'fs/promises';
 import constants from 'fs';
 
-fs.access('1.txt', constants.F_OK).then(() => fs.readFile('1.txt', 'utf8').then(data=>console.log(data))).catch(er => console.log(er));
+fs.access('1.txt', constants.F_OK).then(() => fs.readFile('1.txt', 'utf8').then(data => console.log(data))).catch(er => console.log(er));
+
+try {
+  await fs.access('1.txt', constants.R_Ok);
+  console.log('can read');
+} catch (e) {
+  console.error('cannot read');
+}
+
+try {
+  await fs.access('1.txt', constants.W_Ok);
+  console.log('can write');
+} catch (e) {
+  console.error('cannot write');
+}
+
+try {
+  await fs.access('1.txt', constants.R_Ok | constants.W_Ok);
+  console.log('can access');
+} catch (e) {
+  console.error('cannot access');
+}
