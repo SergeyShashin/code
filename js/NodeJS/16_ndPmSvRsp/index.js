316e62f60ada4087f3b58d4bf0da2eb0aef54a13
SergeyShashin <s9997910@yandex.ru>
2025-02-12 17:14:22 +0300

    Code. NodeJS16+ 16_ndPmSvRsp done.

---
+++ js/NodeJS/16_ndPmSvRsp/index.js
@@ -0,0 +1,22 @@
+import { write } from 'fs';
+import http from 'http';
+
+let date = new Date();
+let time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+// http.createServer((request, response) => {
+//   response.setHeader('Content-Type', 'text/html');
+//   response.statusCode = 200;
+//   response.write('<h1>Welcome WORLD)</h1>');
+//   response.write(`<p>${time} </p>`);
+//   response.end();
+// }).listen(3000);
+
+http.createServer((request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+  response.statusCode = 200;
+  response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Language': 'ru' })
+  response.write('<h1>Welcome WORLD)</h1>');
+  response.write(`<p>${time} </p>`);
+  response.end();
+}).listen(3000);
