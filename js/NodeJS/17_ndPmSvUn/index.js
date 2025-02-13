0b62a9fb96a9f0b613e59908127f38a95cf3c2ca
SergeyShashin <s9997910@yandex.ru>
2025-02-13 09:09:30 +0300

    Code. NodeJS16+ 17_ndPmSvUn done.

--- js/NodeJS/17_ndPmSvUn/index.js
+++ js/NodeJS/17_ndPmSvUn/index.js
@@ -8,7 +8,7 @@ let i = 0;
  */
 
 http.createServer((request, responce) => {
-  responce.writeHead(200, {'Content-Type': 'text/html', 'Content-Language': 'ru'});
-  i===100 ?  responce.write(`<h1>Quantity ${i++}</h1>`):responce.write(`<h1>${i++}</h1>`);
+  responce.writeHead(200, { 'Content-Type': 'text/html', 'Content-Language': 'ru' });
+  i === 100 ? responce.write(`<h1>Quantity ${i++}</h1>`) : responce.write(`<h1>${i++}</h1>`);
   responce.end();
 }).listen(3000);
