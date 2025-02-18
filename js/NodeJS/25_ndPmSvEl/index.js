17b43ce909ed20d092dc1701534f42d92446b58a
SergeyShashin <s9997910@yandex.ru>
2025-02-18 17:23:07 +0300

    Code. NodeJS16+ 25_ndPmSvEl start.

---
+++ js/NodeJS/25_ndPmSvEl/index.js
@@ -0,0 +1,42 @@
+import http, { request } from 'http';
+import fs from 'fs';
+
+/**
+ * 1. Разберите приведенный мною код. Напишите текст, в котором вы объясните работу этого кода.
+ * 2. Добавьте этот код к коду сервера, созданному вами в предыдущем уроке.
+ * 3. Возьмите созданный вами сайт из 6-ти страниц и разделите шаблон сайта на элементы. 
+ */
+
+
+http.createServer(
+  async (request, response) => {
+    let encoding = 'utf8';
+    let statusResponce = 200;
+    let typeContent = 'text/html';
+    let requestUrl = request.url;
+    let lpath = 'layout.html';
+    let tpath;
+    let cpath;
+
+    if (requestUrl === '/') {
+      cpath = `content.html`;
+      tpath = `title.html`;
+    } else if (/^\/page/.test(requestUrl)) {
+      tpath = `.${requestUrl}/title.html`;
+      cpath = `.${requestUrl}/content.html`;
+    }
+
+    console.log(lpath);
+
+    let layout = await fs.promises.readFile(lpath, encoding);
+    let title = await fs.promises.readFile(tpath, encoding);
+    let content = await fs.promises.readFile(cpath, encoding);
+
+    layout = layout.replace(/\{% get title %\}/, title);
+    layout = layout.replace(/\{% get content %\}/, content);
+
+    response.writeHead(statusResponce, { 'Content-type': typeContent });
+    response.write(layout);
+    response.end();
+  }
+).listen(3000);
