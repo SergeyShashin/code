a448103ab9388a6924e9364f4ec733ec04cb99d6
SergeyShashin <s9997910@yandex.ru>
2025-02-20 09:43:57 +0300

    Code. NodeJS16+ 25_ndPmSvEl updated.

--- js/NodeJS/25_ndPmSvEl/index.js
+++ js/NodeJS/25_ndPmSvEl/index.js
@@ -7,7 +7,6 @@ import fs from 'fs';
  * 3. Возьмите созданный вами сайт из 6-ти страниц и разделите шаблон сайта на элементы. 
  */
 
-
 http.createServer(
   async (request, response) => {
     let encoding = 'utf8';
@@ -17,6 +16,10 @@ http.createServer(
     let lpath = 'layout.html';
     let tpath = 'title.html';
     let cpath = 'content.html';
+    let asidePath = './elems/aside.html';
+    let footerPath = './elems/footer.html';
+    let headerPath = './elems/header.html';
+    let menuPath = './elems/menu.html';
 
     if (/^\/page/.test(requestUrl)) {
       tpath = `.${requestUrl}/title.html`;
@@ -27,16 +30,24 @@ http.createServer(
 
     let title = await fs.promises.readFile(tpath, encoding);
     let content = await fs.promises.readFile(cpath, encoding);
+    let aside = await fs.promises.readFile(asidePath, encoding);
+    let header = await fs.promises.readFile(headerPath, encoding);
+    let menu = await fs.promises.readFile(menuPath, encoding);
+    let footer = await fs.promises.readFile(footerPath, encoding);
 
     layout = layout.replace(/\{% get title %\}/, title);
     layout = layout.replace(/\{% get content %\}/, content);
+    layout = layout.replace(/\{% get elem 'aside' %\}/, aside);
+    layout = layout.replace(/\{% get elem 'header' %\}/, header);
+    layout = layout.replace(/\{% get elem 'menu' %\}/, menu);
+    layout = layout.replace(/\{% get elem 'footer' %\}/, footer);
 
-    layout = layout.replace(/\{% get elem '(.+?)' %\}/g, async (match0, match1) => {
-      let result = await fs.promises.readFile(`./elems/${match1}.html`, encoding)
-      console.log(typeof result);
-      // console.log(layout);
-      return result;
-    });
+    // layout = layout.replace(/\{% get elem '(.+?)' %\}/g, async (match0, match1) => {
+    //   let result = await fs.promises.readFile(`./elems/${match1}.html`, encoding)
+    //   console.log(typeof result);
+    //   // console.log(layout);
+    //   return result;
+    // });
 
     response.writeHead(statusResponce, { 'Content-type': typeContent });
     response.write(layout);
