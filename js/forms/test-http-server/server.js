93a372ac1b0a2674a19c85d9f77f2ecb896233c1
SergeyShashin <s9997910@yandex.ru>
2025-02-27 13:59:59 +0300

    Code. forms updated.

--- js/forms/test-http-server/server.js
+++ js/forms/test-http-server/server.js
@@ -1,7 +1,15 @@
+let obj = { 1: 'a', 2: 'b', 3: 'c' };
+
 export default {
-  '/handler/': function ({get, post}) {
+  '/handler/': function ({ get, post }) {
     console.log(get);
-    console.log(post);
-    return 'form data received'
+    // console.log(post);
+    // return (Number(post.num1)+Number(post.num2)+Number(post.num3)+Number(post.num4)+Number(post.num5))/5
+    // return `${post.date}`
+    // let login = 'admin';
+    // let password = 'admin'
+    // return `Login ${post.login === login}. Password ${post.password === password}.`
+    // return get.num ** 2
+    return obj[get.key]
   }
 }
