9ea5816faa5ece82edd7d0948135b23b5d0b7ae2
SergeyShashin <s9997910@yandex.ru>
2025-03-03 14:09:14 +0300

    Code. AJAX updated.

--- js/AJAX/test-http-server/server.js
+++ js/AJAX/test-http-server/server.js
@@ -21,6 +21,25 @@ export default {
   },
 
   '/getSumUsePost/': function ({ post }) {
-    return Number(post.n1) + Number(post.n2)+  Number(post.n3)
-  }
+    return Number(post.n1) + Number(post.n2) + Number(post.n3)
+  },
+
+  '/getSumUsePostFromInputs/': function ({ post }) {
+    return Number(post.n0) + Number(post.n1) + Number(post.n2)
+  },
+
+  '/getDataFromPost/': function ({ post }) {
+    return post.a + post.b + post.c
+  },
+
+  '/sendFormUseFormData/': function ({ post }) {
+    let numbers = Object.values(post);
+
+    return numbers.reduce((sum, number) => sum + Number(number), 0) / numbers.length;
+  },
+
+  '/getSumUsePostAndJson/': function ({ body }) {
+    return JSON.parse(body).reduce((sum, number) => sum + number, 0)
+  },
+
 }
