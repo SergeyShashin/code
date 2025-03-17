acc685e4d252d46dd523c77949e6edeba9c57702
SergeyShashin <s9997910@yandex.ru>
2025-03-17 15:01:00 +0300

    Code. 46_jsOpAdCM.html done.

---
+++ js/oop/46_jsOpAdCM.html/Employee.js
@@ -0,0 +1,5 @@
+export default class Employee {
+  welcome() {
+    console.log('Welcome)');
+  }
+}
---
+++ js/oop/46_jsOpAdCM.html/index.js
@@ -0,0 +1,5 @@
+import Employee from './Employee.js';
+
+let employee = new Employee;
+
+employee.welcome();
