d1dc1fb7f59f88bd0dfbad97457b2f65fb266941
SergeyShashin <s9997910@yandex.ru>
2025-07-02 13:18:12 +0300

    JS NodeJS020725 start.

---
+++ js/NodeJS 020725/app.js
@@ -0,0 +1,8 @@
+console.log('Welcome WORLD!');
+
+// let a = 8;
+// let b = 8;
+
+// console.log(a + b);
+let timer=0
+setInterval(() => { let date = new Date(); console.log(date.getHours(), date.getMinutes(), date.getSeconds(), timer++); }, 1000);
