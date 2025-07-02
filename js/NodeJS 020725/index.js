79840dd15aa1742aa435942a75dc349b4d863fad
SergeyShashin <s9997910@yandex.ru>
2025-07-02 17:09:45 +0300

    JS NodeJS020725 update.

---
+++ js/NodeJS 020725/index.js
@@ -0,0 +1,8 @@
+import { square, cube } from './math.js';
+import { open, read, close } from 'fs';
+// import _ from 'underscore';
+// import _ from 'lodash';
+
+// console.log(square(8));
+// console.log(cube(8));
+// console.log(_.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 }));
