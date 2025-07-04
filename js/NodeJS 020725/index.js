d85e345714a21ef0ddf85d03f2468e27a3e74fd3
SergeyShashin <s9997910@yandex.ru>
2025-07-04 10:56:06 +0300

    JS NodeJS020725 update.

--- js/NodeJS 020725/index.js
+++ js/NodeJS 020725/index.js
@@ -57,8 +57,25 @@ import { open, read, close, readFileSync, writeFileSync, readFile, writeFile } f
 // readFile('./txt/a.txt', 'utf8', (err, data) => console.log(err ? err : data ** 2));
 // console.log('!!!');
 
-let pathDir = './txt/';
+// let pathDir = './txt/';
 
-for (let i = 1; i < 11; i++) {
-  writeFile(pathDir + i + '.txt', String(i), err => console.log(err ? err : ''));
-}
+// for (let i = 1; i < 11; i++) {
+//   writeFile(pathDir + i + '.txt', String(i), err => console.log(err ? err : ''));
+// }
+
+
+let pathDir = './txt/';
+let namesFiles = ['a.txt', 'b.txt', 'c.txt'];
+let fileEnconding = 'utf8';
+let sum = 0;
+
+readFile(pathDir + namesFiles[0], fileEnconding, (err0, content0) => {
+  content0 ? sum += Number(content0) : console.log(err0);
+  readFile(pathDir + namesFiles[1], fileEnconding, (err1, content1) => {
+    content1 ? sum += Number(content1) : console.log(err1);
+    readFile(pathDir + namesFiles[2], fileEnconding, (err2, content2) => {
+      content2 ? sum += Number(content2) : console.log(err2);
+      console.log(sum);
+    });
+  });
+});
