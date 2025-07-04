18cd6c769e470e5250be5e020be839f949a7494c
SergeyShashin <s9997910@yandex.ru>
2025-07-04 10:59:12 +0300

    JS NodeJS020725 update.

--- js/NodeJS 020725/index.js
+++ js/NodeJS 020725/index.js
@@ -65,7 +65,7 @@ import { open, read, close, readFileSync, writeFileSync, readFile, writeFile } f
 
 
 let pathDir = './txt/';
-let namesFiles = ['a.txt', 'b.txt', 'c.txt'];
+let namesFiles = ['a.txt', 'b.txt', 'c.txt', 'a.txt', 'b.txt'];
 let fileEnconding = 'utf8';
 let sum = 0;
 
@@ -75,7 +75,13 @@ readFile(pathDir + namesFiles[0], fileEnconding, (err0, content0) => {
     content1 ? sum += Number(content1) : console.log(err1);
     readFile(pathDir + namesFiles[2], fileEnconding, (err2, content2) => {
       content2 ? sum += Number(content2) : console.log(err2);
-      console.log(sum);
+      readFile(pathDir + namesFiles[3], fileEnconding, (err3, content3) => {
+        content3 ? sum += Number(content3) : console.log(err3);
+        readFile(pathDir + namesFiles[4], fileEnconding, (err4, content4) => {
+          content4 ? sum += Number(content4) : console.log(err4);
+          console.log(sum*22+8);
+        });
+      });
     });
   });
 });
