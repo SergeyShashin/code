afdce2d617f8efa1945d8511fc7ff23f66af973d
SergeyShashin <s9997910@yandex.ru>
2025-07-04 11:21:11 +0300

    JS NodeJS020725 update.

--- js/NodeJS 020725/index.js
+++ js/NodeJS 020725/index.js
@@ -64,24 +64,31 @@ import { open, read, close, readFileSync, writeFileSync, readFile, writeFile } f
 // }
 
 
-let pathDir = './txt/';
-let namesFiles = ['a.txt', 'b.txt', 'c.txt', 'a.txt', 'b.txt'];
-let fileEnconding = 'utf8';
-let sum = 0;
+// let pathDir = './txt/';
+// let namesFiles = ['a.txt', 'b.txt', 'c.txt', 'a.txt', 'b.txt'];
+// let fileEnconding = 'utf8';
+// let sum = 0;
 
-readFile(pathDir + namesFiles[0], fileEnconding, (err0, content0) => {
-  content0 ? sum += Number(content0) : console.log(err0);
-  readFile(pathDir + namesFiles[1], fileEnconding, (err1, content1) => {
-    content1 ? sum += Number(content1) : console.log(err1);
-    readFile(pathDir + namesFiles[2], fileEnconding, (err2, content2) => {
-      content2 ? sum += Number(content2) : console.log(err2);
-      readFile(pathDir + namesFiles[3], fileEnconding, (err3, content3) => {
-        content3 ? sum += Number(content3) : console.log(err3);
-        readFile(pathDir + namesFiles[4], fileEnconding, (err4, content4) => {
-          content4 ? sum += Number(content4) : console.log(err4);
-          console.log(sum*22+8);
-        });
-      });
-    });
-  });
-});
+// readFile(pathDir + namesFiles[0], fileEnconding, (err0, content0) => {
+//   content0 ? sum += Number(content0) : console.log(err0);
+//   readFile(pathDir + namesFiles[1], fileEnconding, (err1, content1) => {
+//     content1 ? sum += Number(content1) : console.log(err1);
+//     readFile(pathDir + namesFiles[2], fileEnconding, (err2, content2) => {
+//       content2 ? sum += Number(content2) : console.log(err2);
+//       readFile(pathDir + namesFiles[3], fileEnconding, (err3, content3) => {
+//         content3 ? sum += Number(content3) : console.log(err3);
+//         readFile(pathDir + namesFiles[4], fileEnconding, (err4, content4) => {
+//           content4 ? sum += Number(content4) : console.log(err4);
+//           console.log(sum*22+8);
+//         });
+//       });
+//     });
+//   });
+// });
+
+let pathDir = './txt/';
+let nameFile = 'newFile.txt';
+let fileEnconding = 'utf8';
+
+readFile(pathDir + nameFile, fileEnconding, (err, content) =>
+  err ? console.log(err) : writeFile(pathDir + nameFile, `${content ** 2}`, errWrite => errWrite ? console.log(errWrite) : ''));
