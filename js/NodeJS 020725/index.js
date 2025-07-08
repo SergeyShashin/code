a79145ae0ef4358e862e2e7e758377e0a1dac81d
SergeyShashin <s9997910@yandex.ru>
2025-07-08 18:16:18 +0300

    JS NodeJS020725 update.

--- js/NodeJS 020725/index.js
+++ js/NodeJS 020725/index.js
@@ -289,8 +289,11 @@ import http from 'http';
 //   response.end();
 // }).listen(3000);
 
+let i = 0;
+
 http.createServer((request, response) => {
+  console.log(request.url, request.method, request.headers);
   response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Language': 'ru' });
-  response.write('{}');
+  response.write(`${++i}`);
   response.end();
 }).listen(3000);
