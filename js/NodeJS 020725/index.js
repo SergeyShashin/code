d7b9ad036bca3d3c8ae97c4bb7884d52b36c4891
SergeyShashin <s9997910@yandex.ru>
2025-07-09 09:20:23 +0300

    JS NodeJS020725 update.

--- js/NodeJS 020725/index.js
+++ js/NodeJS 020725/index.js
@@ -299,3 +299,14 @@ import http from 'http';
 //   response.write(`${++i}`);
 //   response.end();
 // }).listen(3000);
+
+
+http.createServer((request, response) => {
+	if (request.url != '/favicon.ico'){
+		console.log(request.url); // теперь выполнится один раз
+		
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('text');
+		response.end();
+	}
+}).listen(3000);
