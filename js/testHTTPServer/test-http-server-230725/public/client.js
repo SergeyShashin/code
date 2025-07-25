8f9947f399a110b029e9c16d8a63e5b643721cde
SergeyShashin <s9997910@yandex.ru>
2025-07-25 13:22:36 +0300

    JS. Code. Updated 250725.

--- js/testHTTPServer/test-http-server-230725/public/client.js
+++ js/testHTTPServer/test-http-server-230725/public/client.js
@@ -28,6 +28,24 @@ alert('welcome');
 
 // });
 
+// let divEl = document.querySelector('div');
+// let counterPages = 0;
+
+// document.body.addEventListener('click', e => {
+//   let pages = ['ajax', 'ajax1', 'ajax2', 'ajax3', 'ajax4', 'ajax5'];
+//   let extension = 'html';
+//   if (e.target.tagName === 'BUTTON') {
+//     console.log(counterPages);
+//     fetch(`/${pages[counterPages]}.${extension}`).then(response => {
+//       response.ok
+//         ? (response.text()).then(response => { divEl.innerHTML = response; counterPages = counterPages === pages.length - 1 ? 0 : counterPages + 1 })
+//         : console.log('File or link, or code needs to be improved.');
+//     });
+//   }
+
+// });
+
+
 let divEl = document.querySelector('div');
 let counterPages = 0;
 
@@ -35,12 +53,13 @@ document.body.addEventListener('click', e => {
   let pages = ['ajax', 'ajax1', 'ajax2', 'ajax3', 'ajax4', 'ajax5'];
   let extension = 'html';
   if (e.target.tagName === 'BUTTON') {
-    console.log(counterPages);
     fetch(`/${pages[counterPages]}.${extension}`).then(response => {
-      response.ok
-        ? (response.text()).then(response => { divEl.innerHTML = response; counterPages = counterPages === pages.length - 1 ? 0 : counterPages + 1 })
-        : console.log('File or link, or code needs to be improved.');
-    });
+      if (response.ok) {
+        (response.text()).then(response => { divEl.innerHTML = response; counterPages = counterPages === pages.length - 1 ? 0 : counterPages + 1 })
+      } else {
+        throw new Error('404 или 500')
+      }
+    }).catch(error => alert(error));
   }
 
 });
