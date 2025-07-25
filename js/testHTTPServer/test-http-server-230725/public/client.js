b9cddbadf4a6bbfc4fc659812f6017554c6f07d7
SergeyShashin <s9997910@yandex.ru>
2025-07-25 13:59:47 +0300

    JS. Code. Updated 250725.

--- js/testHTTPServer/test-http-server-230725/public/client.js
+++ js/testHTTPServer/test-http-server-230725/public/client.js
@@ -46,20 +46,33 @@ alert('welcome');
 // });
 
 
-let divEl = document.querySelector('div');
-let counterPages = 0;
+// let divEl = document.querySelector('div');
+// let counterPages = 0;
 
-document.body.addEventListener('click', e => {
-  let pages = ['ajax', 'ajax1', 'ajax2', 'ajax3', 'ajax4', 'ajax5'];
-  let extension = 'html';
-  if (e.target.tagName === 'BUTTON') {
-    fetch(`/${pages[counterPages]}.${extension}`).then(response => {
-      if (response.ok) {
-        (response.text()).then(response => { divEl.innerHTML = response; counterPages = counterPages === pages.length - 1 ? 0 : counterPages + 1 })
-      } else {
-        throw new Error('404 или 500')
-      }
-    }).catch(error => alert(error));
+// document.body.addEventListener('click', e => {
+//   let pages = ['ajax', 'ajax1', 'ajax2', 'ajax3', 'ajax4', 'ajax5'];
+//   let extension = 'html';
+//   if (e.target.tagName === 'BUTTON') {
+//     fetch(`/${pages[counterPages]}.${extension}`).then(response => {
+
+//       console.log(response.headers.get('Content-Length'));
+
+//       if (response.ok) {
+//         (response.text()).then(response => { divEl.innerHTML = response; counterPages = counterPages === pages.length - 1 ? 0 : counterPages + 1 })
+//       } else {
+//         throw new Error('404 или 500')
+//       }
+//     }).catch(error => alert(error));
+//   }
+
+// });
+
+fetch('/data.json').then(response => response.json()).then(response => {
+  // let data = JSON.parse(response);
+
+  for (let el of Object.values(response)) {
+    let liEl = document.createElement('li');
+    liEl.textContent = el;
+    document.body.appendChild(liEl);
   }
-
-});
+})
