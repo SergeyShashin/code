a0872aa22d20bee2baa79f718255f8fd1ce4f170
SergeyShashin <s9997910@yandex.ru>
2025-10-23 10:30:05 +0300

    JS code.  47_jsOpAdIh 231025 done.

---
+++ js/oop/47_jsOpAdIh 231025/Programmer.js
@@ -0,0 +1,7 @@
+import Employee from "./Employee.js";
+
+export default class Programmer extends Employee {
+  welcome() {
+    console.log('Welcome)');
+  }
+}
---
+++ js/oop/47_jsOpAdIh 231025/index.js
@@ -0,0 +1,6 @@
+// import Employee from "./Employee.js";
+import Programmer from "./Programmer.js";
+
+let programmer = new Programmer;
+
+console.log(programmer.welcome());
