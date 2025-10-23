ff462739d2d792ba1f1f5e038eae6f15f676a7f6
SergeyShashin <s9997910@yandex.ru>
2025-10-23 09:47:14 +0300

    JS code. oop/classes-modules. 46_jsOpAdCM 231025 done.

---
+++ js/oop/46_jsOpAdCM 231025/Employee.js
@@ -0,0 +1,5 @@
+export default class Employee{
+  sayWelcome(){
+    console.log('Welcome WORLD!)');
+  }
+}
---
+++ js/oop/46_jsOpAdCM 231025/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import Employee from './Employee.js';
+
+/*
+1. Разместите класс Employee в отдельном файле.
+2. В файле index.js создайте объект этого класса.
+*/
+
+const employee = new Employee();
